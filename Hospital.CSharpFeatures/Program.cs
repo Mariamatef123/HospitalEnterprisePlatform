3b0c080bd684c6710413b69a151dc1da7538831d
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-08-21 18:23:45 +0300

    chore: initialize hospital enterprise platform

---
+++ Hospital.CSharpFeatures/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospital.CSharpFeatures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Hospital.Plugins.Statistics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospital.Plugins.Statistics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
