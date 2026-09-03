7b5757d66a1c8f08b6459d252594ee225ff201bc
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-09-03 14:24:55 +0300

    test(domain): add receptionist tests

---
+++ tests/Hospital.Domain.Tests/Employees/ReceptionistTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Hospital.Domain.Employees;
+using Hospital.Domain.Employees.ValueObjects;
+
+namespace Hospital.Domain.Tests.Employees
+{
+    public class ReceptionistTests
+    {
+        private Receptionist CreateReceptionist()
+        {
+            return new Receptionist(
+                Guid.NewGuid(),
+                new NationalId("30203020106543"),
+                new PersonName("Mariam", "Atef"),
+                new PhoneNumber("01271689560"),
+                Guid.NewGuid()
+            );
+        }
+
+        [Fact]
+        public void Receptionist_AssignedToHospitalEmployee_IsReceptionist()
+        {
+            HospitalEmployee employee = CreateReceptionist();
+
+            employee.Should().BeOfType<Receptionist>();
+        }
+
+        [Fact]
+        public void Receptionist_Should_Have_Correct_Permissions()
+        {
+            var receptionist = CreateReceptionist();
+
+            var permissions = receptionist.GetRolePermissions();
+
+            permissions.Should().Contain(EmployeePermission.RegisterPatient);
+            permissions.Should().Contain(EmployeePermission.ScheduleAppointment);
+        }
+    }
+}
