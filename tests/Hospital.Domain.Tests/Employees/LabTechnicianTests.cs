f1b6b1c02139aa736120f15b60f396c916708792
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-09-03 14:18:03 +0300

    test(domain): add lab technician tests

---
+++ tests/Hospital.Domain.Tests/Employees/LabTechnicianTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Hospital.Domain.Employees;
+using Hospital.Domain.Employees.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Domain.Tests.Employees
+{
+    public class LabTechnicianTests
+    {
+ 
+            private LabTechnician CreateLabTechnician()
+            {
+                return new LabTechnician(
+                    Guid.NewGuid(),
+                    new NationalId("30203020106543"),
+                    new PersonName("Mariam", "Atef"),
+                    new PhoneNumber("01271689560"),
+                    Guid.NewGuid()
+                );
+            }
+
+            [Fact]
+            public void LabTechnician_AssignedToHospitalEmployee_IsLabTechnician()
+            {
+                HospitalEmployee employee = CreateLabTechnician();
+
+                employee.Should().BeOfType<LabTechnician>();
+            }
+
+            [Fact]
+            public void LabTechnician_Should_Have_Correct_Permissions()
+            {
+                var labTechnician = CreateLabTechnician();
+
+                var permissions = labTechnician.GetRolePermissions();
+
+                permissions.Should().Contain(EmployeePermission.ProcessLabTests);
+       
+            }
+        }
+   
+}
