797cf7d05912e4522f5cf05fe8e810a47a62b90b
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-09-03 14:12:56 +0300

    test(domain): add billing clerk tests

---
+++ tests/Hospital.Domain.Tests/Employees/BillingClerkTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Hospital.Domain.Employees;
+using Hospital.Domain.Employees.ValueObjects;
+
+namespace Hospital.Domain.Tests.Employees
+{
+    public class BillingClerkTests
+    {
+        private BillingClerk CreateBillingClerk()
+        {
+            return new BillingClerk(
+                Guid.NewGuid(),
+                new NationalId("30203020106543"),
+                new PersonName("Mariam", "Atef"),
+                new PhoneNumber("01271689560"),
+                Guid.NewGuid()
+            );
+        }
+
+        [Fact]
+        public void BillingClerk_AssignedToHospitalEmployee_IsBillingClerk()
+        {
+            HospitalEmployee employee = CreateBillingClerk();
+
+            employee.Should().BeOfType<BillingClerk>();
+        }
+
+        [Fact]
+        public void BillingClerk_Should_Have_Correct_Permissions()
+        {
+            var billingClerk = CreateBillingClerk();
+
+            var permissions = billingClerk.GetRolePermissions();
+
+            permissions.Should().Contain(EmployeePermission.ProcessBilling);
+        }
+    }
+}
