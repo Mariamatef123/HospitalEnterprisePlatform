da28ded985f2d69975eb9177423b3bb6c5989361
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-09-03 14:29:47 +0300

    test(domain): add pharmacist tests

---
+++ tests/Hospital.Domain.Tests/Employees/PharmacistTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Hospital.Domain.Employees;
+using Hospital.Domain.Employees.ValueObjects;
+
+namespace Hospital.Domain.Tests.Employees
+{
+    public class PharmacistTests
+    {
+        private Pharmacist CreatePharmacist()
+        {
+            return new Pharmacist(
+                Guid.NewGuid(),
+                new NationalId("30203020106543"),
+                new PersonName("Mariam", "Atef"),
+                new PhoneNumber("01271689560"),
+                Guid.NewGuid()
+            );
+        }
+
+        [Fact]
+        public void Pharmacist_AssignedToHospitalEmployee_IsPharmacist()
+        {
+            HospitalEmployee employee = CreatePharmacist();
+
+            employee.Should().BeOfType<Pharmacist>();
+        }
+
+        [Fact]
+        public void Pharmacist_Should_Have_Correct_Permissions()
+        {
+            var pharmacist = CreatePharmacist();
+
+            var permissions = pharmacist.GetRolePermissions();
+
+            permissions.Should().Contain(
+                EmployeePermission.ManageMedicationInventory);
+        }
+    }
+}
